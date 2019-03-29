24a0261466a5286351d4700bf14530d8575349e1
Nagy Róbert <nagyrobert0213@gmail.com>
2019-03-29 05:07:54 +0100

    fixed setSiteName function

--- config.php
+++ config.php
@@ -5,12 +5,19 @@ class Config
 
     public static function setSiteName()
     {
+        if (!empty($_POST["sitename"])) {
             if (strpos($_POST["sitename"], '.myshopify.com') !== false) {
                 $siteName = $_POST["sitename"];
                 $tempSiteName = strval($siteName);
                 $_SESSION['name'] = $tempSiteName;
                 return $_SESSION['name'];
+            } else {
+                echo "<script type= 'text/javascript'>alert('Invalid URL!');</script>";
+                self::deleteCookies();
+                header("Refresh:0; url=index.php?_ijt=o1gqfk2t3tkbr1ltv51bus7lcb");
+                die();
             }
+        }
         return "Site name is not specified!";
     }
 
