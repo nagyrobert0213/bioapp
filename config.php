8b11b842594b62358cc64c8852491186f7d218c3
Nagy Róbert <nagyrobert0213@gmail.com>
2019-03-29 05:01:45 +0100

    created function setSiteName in config

--- config.php
+++ config.php
@@ -1,1 +1,16 @@
 <?php
+
+class Config
+{
+
+    public static function setSiteName()
+    {
+            if (strpos($_POST["sitename"], '.myshopify.com') !== false) {
+                $siteName = $_POST["sitename"];
+                $tempSiteName = strval($siteName);
+                $_SESSION['name'] = $tempSiteName;
+                return $_SESSION['name'];
+            }
+        return "Site name is not specified!";
+    }
+}
