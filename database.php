77077ea5dd1f3bd07a5592941cd0631426d3fb94
Nagy Róbert <nagyrobert0213@gmail.com>
2019-03-29 05:28:02 +0100

    added getData function to database

--- database.php
+++ database.php
@@ -1,1 +1,18 @@
 <?php
+
+class Database
+{
+    public $conn;
+
+    public static function getData()
+    {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+
+        $conn = new mysqli($server, $username, $password, $db);
+        return $conn;
+    }
+}
