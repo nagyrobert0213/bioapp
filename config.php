98ca74251fe01f23f061f4ef42116ddc27822aaa
Nagy Róbert <nagyrobert0213@gmail.com>
2019-03-29 05:03:14 +0100

    created function deleteCookies in config

--- config.php
+++ config.php
@@ -13,4 +13,19 @@ class Config
             }
         return "Site name is not specified!";
     }
+
+    public static function deleteCookies()
+    {
+        if (isset($_SERVER['HTTP_COOKIE'])) {
+            $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+            foreach ($cookies as $cookie) {
+                $parts = explode('=', $cookie);
+                $name = trim($parts[0]);
+                setcookie($name, '', time() - 1000);
+                setcookie($name, '', time() - 1000, '/');
+            }
+            header("Refresh:0; url=index.php?_ijt=o1gqfk2t3tkbr1ltv51bus7lcb");
+            die();
+        }
+    }
 }
