d69ce526d0cba93688c36be17587ead64d83c075
Nagy Róbert <nagyrobert0213@gmail.com>
2019-03-29 05:17:57 +0100

    added createCustomer function with validation to sendapi

--- sendapi.php
+++ sendapi.php
@@ -1,1 +1,21 @@
 <?php
+
+class SendApi
+{
+
+    public static function createCustomer($shopify)
+    {
+            try {
+                for ($i = 0; $i <= 30; $i++) {
+                    $newCustomer = array(
+                        "email" => "nagyrobert$i@gmail.com",
+                        "first_name" => "Roberts",
+                        "last_name" => "Nagey"
+                    );
+                    $shopify->Customer->post($newCustomer);
+                }
+            } catch (Exception $exception) {
+                echo '<br><br>Customer already created.';
+            }
+    }
+}
